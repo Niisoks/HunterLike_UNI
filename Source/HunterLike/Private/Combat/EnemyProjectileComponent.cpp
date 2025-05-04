831c982f1c029509c8b9565d0a145eefeaa57bf3
Cole Hall <colebrhall@gmail.com>
2025-05-04 03:44:08 +0100

    Aim projectile at player

--- Source/HunterLike/Private/Combat/EnemyProjectileComponent.cpp
+++ Source/HunterLike/Private/Combat/EnemyProjectileComponent.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Combat/EnemyProjectileComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values for this component's properties
 UEnemyProjectileComponent::UEnemyProjectileComponent()
@@ -40,7 +41,16 @@ void UEnemyProjectileComponent::SpawnProjectile(FName ComponentName, TSubclassOf
 
 	FVector SpawnLocation{ SpawnPointComp->GetComponentLocation() };
 
-	GetWorld()->SpawnActor(ProjectileClass, &SpawnLocation);
+	FVector PlayerLocation{ GetWorld()->GetFirstPlayerController()
+		->GetPawn()
+		->GetActorLocation()
+	};
+
+	FRotator SpawnRotation{
+		UKismetMathLibrary::FindLookAtRotation(SpawnLocation, PlayerLocation)
+	};
+
+	GetWorld()->SpawnActor(ProjectileClass, &SpawnLocation, &SpawnRotation);
 
 }
 
